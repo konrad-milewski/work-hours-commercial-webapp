b854c31be248e85b2babcc20cff01269aea210af
kondri <kondri@gmail.com>
2022-05-19 21:07:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { Container } from "react-bootstrap";
 import { useState } from "react";
 
 function App() {
-
+  
   return (
    
 <Router>
--- src/layouts/main/main.js
+++ src/layouts/main/main.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import AddEmployerModal from '../../components/add-employer-modal'
-import EditHoursModal from '../../components/edit-hours-modal/editHoursModal'
-import Navigation from '../../components/navigation'
-import UsersTable from '../../components/users-table'
+import React, { useEffect, useState } from "react";
+import AddEmployerModal from "../../components/add-employer-modal";
+import EditHoursModal from "../../components/edit-hours-modal/editHoursModal";
+import Navigation from "../../components/navigation";
+import UsersTable from "../../components/users-table";
+import axios from "axios";
 
 export default function Main() {
+  const [users, setusers] = useState({})
+  
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/users`)
+      .then(function (response) {
+      
+        setusers(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
+  }, []);
+
   return (
     <>
-    {/* <AddEmployerModal/> */}
+      {/* <AddEmployerModal/> */}
       {/* <EditHoursModal/> */}
-     
-    <div className='mt-2'>
-    <Navigation/>
-    <Navigation/>
-    </div>
-   
-    <UsersTable/>
-    <UsersTable/>
-  
+
+      <div className="mt-2">
+        <Navigation />
+        <Navigation />
+      </div>
+
+      <UsersTable data={users.logged} name="Zalogowani" />
+      <UsersTable data={users.logout} name="Wylogowani"/>
     </>
-  )
+  );
 }
